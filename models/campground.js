d7cfe414333f23cbb43b8db079db4eab997b3090
Theo <theo.riberti@gmail.com>
2021-03-19 16:04:21 +0000

    Section 56 : added fancy cluster map with nice custom pop-ups
    nice example of tweaking code as you go, setting up cluster sizes, custom pop-ups using virtuals, and working on scripts on the client side

--- models/campground.js
+++ models/campground.js
@@ -9,39 +9,54 @@ const ImageSchema = new Schema({
 
 //res.cloudinary.com/demo/image/upload/w_150,c_scale/sample.jpg
 
+//this allows virtual properties to be passed when converted to json format
+const opts = { toJSON: { virtuals: true } };
+
 ImageSchema.virtual("thumbnail").get(function () {
   return this.url.replace("/upload", "/upload/w_150,c_scale");
 });
 
-const CampgroundSchema = new Schema({
-  title: String,
-  images: [ImageSchema],
-  //standard geojson confs taken from mongoose docs
-  geometry: {
-    type: {
-      type: String, // Don't do `{ location: { type: String } }`
-      enum: ["Point"], // 'location.type' must be 'Point'
-      required: true,
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    images: [ImageSchema],
+    //standard geojson confs taken from mongoose docs
+    geometry: {
+      type: {
+        type: String, // Don't do `{ location: { type: String } }`
+        enum: ["Point"], // 'location.type' must be 'Point'
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
     },
-    coordinates: {
-      type: [Number],
-      required: true,
-    },
-  },
-  price: Number,
-  description: String,
-  location: String,
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  reviews: [
-    {
+    price: Number,
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: "Review",
+      ref: "User",
     },
-  ],
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
+
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+  const briefDescription = this.description.slice(0, 72);
+  return `<h6><a href="/campgrounds/${
+    this._id
+  }">${this.title}</a></h6><p><strong>${this.location}</strong></p><p>${briefDescription}...</p><img class="img-fluid" src="${this.images[0] ? this.images[0].thumbnail : ""}"/>`;
 });
+
+//
 
 //mongoose query middleware to delete all reviews associated to a campground ----note that if the way a campground is deleted is changed, like instead of "findByIdAndDel..." replaceing by the remove method or delete many.., this midware'd stop working
 CampgroundSchema.post("findOneAndDelete", async function (campground) {
--- seeds/index.js
+++ seeds/index.js
@@ -21,29 +21,30 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 250; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
+    const randomCity = cities[random1000];
     const camp = new Campground({
-      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      location: `${randomCity.city}, ${randomCity.state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       author: "604a5d4e12153d263454ffaf",
       //this url is a small api at unsplash that returns a random picture from the specified collection everytime is accessed
       images: [
         {
           url:
-            "https://res.cloudinary.com/ddlgml3yp/image/upload/v1615694685/YelpCamp/q0mmdu2k8mfulgmqitjh.jpg",
-          filename: "YelpCamp/q0mmdu2k8mfulgmqitjh",
+            "https://res.cloudinary.com/ddlgml3yp/image/upload/v1616167011/YelpCamp/sry9mnmr48i6jyoir9ei.jpg",
+          filename: "YelpCamp/sry9mnmr48i6jyoir9ei",
         },
         {
           url:
-            "https://res.cloudinary.com/ddlgml3yp/image/upload/v1615694688/YelpCamp/u624sfpaagnbzb8b1tvr.jpg",
-          filename: "YelpCamp/u624sfpaagnbzb8b1tvr",
+            "https://res.cloudinary.com/ddlgml3yp/image/upload/v1615784032/YelpCamp/gun9df9lqdizcpju3qkq.jpg",
+          filename: "YelpCamp/gun9df9lqdizcpju3qkq",
         },
         {
           url:
-            "https://res.cloudinary.com/ddlgml3yp/image/upload/v1615694692/YelpCamp/xrojir0vomlr4rzghnfd.jpg",
-          filename: "YelpCamp/xrojir0vomlr4rzghnfd",
+            "https://res.cloudinary.com/ddlgml3yp/image/upload/v1615804992/YelpCamp/iexokdch4degygmgt9qr.jpg",
+          filename: "YelpCamp/iexokdch4degygmgt9qr",
         },
       ],
       description:
@@ -51,7 +52,7 @@ const seedDB = async () => {
       price: price,
       geometry: {
         type: "Point",
-        coordinates: [-114.757175843012, 54.6426717391],
+        coordinates: [randomCity.longitude, randomCity.latitude],
       },
     });
     await camp.save();
